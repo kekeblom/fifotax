d473599f176e7954a9ed59ad8ead104a0cf36b62
Kenneth Blomqvist <kekeblom@gmail.com>
2018-01-04 22:32:46 +0200

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
-extern crate csv;
-extern crate chrono;
-extern crate serde;
+
 #[macro_use]
 extern crate serde_derive;
 
-use std::env;
 use std::error::Error;
-use std::fs::File;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry;
-use chrono::prelude::*;
+use std::env;
 
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-struct Transaction {
-    timestamp: DateTime<Utc>,
-    ticker: String, // Id of currency purchased.
-    transaction_type: String, // "Buy" or "Sell".
-    amount: f64, // Amount purchased.
-    subtotal: f64, // Amount * price per coin.
-    fees: f64, // How much was payed in fees.
-    total: f64, // Subtotal + fees.
-    currency: String, // What currency the transaction was payed in.
-    price_per_coin: f64, // Unit price of the purchased currency.
-    id: String
-}
+mod tax;
 
-pub mod transaction_types {
-    pub const BUY: &'static str = "Buy";
-    pub const SELL: &'static str = "Sell";
-    pub const TX: &'static str = "Tx";
-}
+use tax::*;
 
 fn read_arguments() -> Result<String, Box<Error>> {
     match env::args().nth(1) {
@@ -39,97 +16,17 @@ fn read_arguments() -> Result<String, Box<Error>> {
     }
 }
 
-fn read_transactions() -> Result<Vec<Transaction>, Box<Error>> {
-    let file_path = read_arguments()?;
-    let file = File::open(file_path)?;
-    let mut reader = csv::Reader::from_reader(file);
-    let mut transactions = Vec::new();
-    for transaction in reader.deserialize() {
-        transactions.push(transaction?);
-    }
-    Ok(transactions)
-}
-
-fn process_buy(balances: &mut HashMap<String, f64>, transaction: Transaction) {
-    let currency = transaction.currency;
-    let total = transaction.total;
-    let ticker = transaction.ticker;
-    let amount = transaction.amount;
-    assert!(amount > 0_f64);
-    // Deduct total value
-    match balances.entry(currency) {
-        Entry::Vacant(entry) => {
-            entry.insert(-total);
-        },
-        Entry::Occupied(mut entry) => {
-            let mut value = entry.get_mut();
-            *value = *value - total;
-        }
-    }
-    // Add purchased amount to balances.
-    match balances.entry(ticker) {
-        Entry::Vacant(entry) => {
-            entry.insert(amount);
-        },
-        Entry::Occupied(mut entry) => {
-            let mut value = entry.get_mut();
-            *value = *value + amount;
-        }
-    }
-}
-
-fn process_sell(balances: &mut HashMap<String, f64>, transaction: Transaction) {
-    // Add currency to balance.
-    match balances.entry(transaction.currency) {
-        Entry::Vacant(entry) => {
-            entry.insert(transaction.subtotal);
-        },
-        Entry::Occupied(mut entry) => {
-            let mut value = entry.get_mut();
-            *value = *value + transaction.subtotal;
-        }
-    }
-    // Deduct sold asset.
-    match balances.entry(transaction.ticker) {
-        Entry::Vacant(entry) => panic!("Selling {} which is not in balances!", entry.key()),
-        Entry::Occupied(mut entry) => {
-            let mut value = entry.get_mut();
-            *value = *value - transaction.amount;
-        }
-    }
-}
-
-fn process_transaction(balances: &mut HashMap<String, f64>, transaction: Transaction) {
-    // Remove transaction fee from balances.
-    match balances.entry(transaction.ticker) {
-        Entry::Vacant(entry) => panic!("Transacting in {} you don't own.", entry.key()),
-        Entry::Occupied(mut entry) => {
-            let mut value = entry.get_mut();
-            *value = *value - transaction.fees;
-        }
-    }
-}
-
-fn calculate_balance(transactions: Vec<Transaction>) -> HashMap<String, f64> {
-    let balances = transactions.into_iter().fold(HashMap::new(), |mut balances, transaction| {
-        match transaction.transaction_type.as_ref() {
-            transaction_types::BUY => process_buy(&mut balances, transaction),
-            transaction_types::SELL => process_sell(&mut balances, transaction),
-            transaction_types::TX => process_transaction(&mut balances, transaction),
-            _ => panic!("Unkown transaction type encountered!")
-        }
-        balances
-    });
-    balances
-}
-
 fn main() {
-    let mut transactions = read_transactions().expect("Can't read transactions");
-    transactions.sort_by(|t1, t2| t1.timestamp.cmp(&t2.timestamp) );
-    let balances = calculate_balance(transactions);
-    balances.iter().for_each(|currency_balance| {
-        let currency = currency_balance.0;
-        let balance = currency_balance.1;
-        println!("{}: {}", currency, balance);
-    });
+    let file_path = read_arguments().expect("No input filename given");
+    let (profits, balances) = calculate_profits_balances(file_path);
+
+    for (currency, entries) in balances.iter() {
+        let total_in_currency: f64 = entries.into_iter().map(|e| e.amount).sum();
+        println!("{}: {}", currency, total_in_currency);
+    }
+
+    println!("Profits:");
+    for profit in profits.into_iter() {
+        println!("sold currency: {}, amount: {}, sell price: {}, cost: {}", profit.currency, profit.amount_sold, profit.eur_total, profit.cost);
+    }
 }
---
+++ src/tax.rs
@@ -0,0 +1,269 @@
+
+extern crate csv;
+extern crate chrono;
+extern crate itertools;
+
+use std::error::Error;
+use std::fs::File;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use self::chrono::prelude::*;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Transaction {
+    timestamp: DateTime<Utc>,
+    ticker: String, // Id of currency purchased.
+    transaction_type: String, // "Buy" or "Sell".
+    amount: f64, // Amount purchased.
+    subtotal: f64, // Amount * price per coin.
+    fees: f64, // How much was payed in fees.
+    total: f64, // Subtotal + fees.
+    currency: String, // What currency the transaction was payed in.
+    price_per_coin: f64, // Unit price of the purchased currency.
+    id: String
+}
+
+impl Clone for Transaction {
+    fn clone(&self) -> Transaction {
+        Transaction {
+            timestamp: self.timestamp.clone(),
+            ticker: self.ticker.clone(),
+            transaction_type: self.transaction_type.clone(),
+            amount: self.amount,
+            subtotal: self.subtotal,
+            fees: self.fees,
+            total: self.total,
+            currency: self.currency.clone(),
+            price_per_coin: self.price_per_coin.clone(),
+            id: self.id.clone().clone()
+        }
+    }
+}
+
+pub mod transaction_types {
+    pub const BUY: &'static str = "Buy";
+    pub const SELL: &'static str = "Sell";
+    pub const TX: &'static str = "Tx";
+}
+
+
+fn read_transactions(file_path: String) -> Result<Vec<Transaction>, Box<Error>> {
+    let file = File::open(file_path)?;
+    let mut reader = csv::Reader::from_reader(file);
+    let mut transactions = Vec::new();
+    for transaction in reader.deserialize() {
+        transactions.push(transaction?);
+    }
+    Ok(transactions)
+}
+
+pub struct BalanceEntry {
+    pub amount: f64,
+    pub currency: String,
+    pub cost: f64
+}
+
+fn buy_cost(balances: &mut HashMap<String, Vec<BalanceEntry>>, transaction: &Transaction) -> f64 {
+    let cost: f64;
+    if transaction.currency == "EUR" {
+        cost = transaction.total;
+    } else {
+        cost = deduct_balance(balances, &transaction.currency, transaction.total);
+    }
+    cost
+}
+
+fn adjust_balance_buy(balances: &mut HashMap<String, Vec<BalanceEntry>>, transaction: &Transaction) {
+    let cost = buy_cost(balances, transaction);
+    let balance_entry = BalanceEntry {
+        amount: transaction.amount,
+        currency: transaction.ticker.clone(),
+        cost: cost
+    };
+    match balances.entry(transaction.ticker.clone()) {
+        Entry::Occupied(mut entry) => {
+            let balances = entry.get_mut();
+            balances.push(balance_entry);
+        },
+        Entry::Vacant(entry) => {
+            entry.insert(vec![balance_entry]);
+        }
+    }
+}
+
+fn deduct_balance(balances: &mut HashMap<String, Vec<BalanceEntry>>, currency_spent: &String, spent: f64) -> f64 {
+    let balance = balances.get_mut(currency_spent).unwrap();
+
+    let total_balance_in_currency: f64 = balance.into_iter().map(|b| b.amount).sum();
+    assert!(total_balance_in_currency >= spent, "We have a problem.");
+
+    let mut amount_spent = spent;
+    let mut total_cost = 0_f64;
+    while amount_spent != 0_f64 {
+        let balance_entry = balance.iter_mut().find(|b| {
+            b.amount != 0_f64
+        }).unwrap();
+        if balance_entry.amount >= amount_spent {
+            let to_deduct = amount_spent;
+            let sold_fraction: f64 = to_deduct / balance_entry.amount;
+
+            total_cost += sold_fraction * balance_entry.cost;
+            balance_entry.cost = balance_entry.cost * (1_f64 - sold_fraction);
+            // We deduct the amount we spent.
+            balance_entry.amount = balance_entry.amount - amount_spent;
+
+            amount_spent = 0_f64;
+        } else {
+            let to_deduct = balance_entry.amount;
+            total_cost += balance_entry.cost;
+            balance_entry.amount = 0_f64;
+            balance_entry.cost = 0_f64;
+            amount_spent -= to_deduct;
+        }
+    }
+    total_cost
+}
+
+fn adjust_balance_sell(balances: &mut HashMap<String, Vec<BalanceEntry>>, transaction: &Transaction) {
+    let amount_sold = transaction.amount;
+    let amount_owned = balances.get(&transaction.ticker).unwrap().iter().map(|b| b.amount).sum();
+    assert!(amount_sold <= amount_owned, "We have a problem");
+    let amount_earned = transaction.subtotal;
+    let currency_earned = transaction.currency.clone();
+    let currency_sold = &transaction.ticker;
+    let purchase_cost = deduct_balance(balances, currency_sold, amount_sold);
+    let new_balance = BalanceEntry {
+        amount: amount_earned,
+        currency: currency_earned,
+        cost: purchase_cost + transaction.fees
+    };
+    balances.get_mut(&new_balance.currency).unwrap().push(new_balance);
+}
+
+pub struct RealizedProfit {
+    pub date: DateTime<Utc>,
+    pub currency: String,
+    pub amount_sold: f64,
+    pub eur_total: f64,
+    pub cost: f64
+}
+
+impl RealizedProfit {
+    pub fn profit(&self) -> f64 {
+        self.eur_total - self.cost
+    }
+}
+
+fn calculate_profit(balances: &mut HashMap<String, Vec<BalanceEntry>>, transaction: &Transaction) -> RealizedProfit {
+    let to_sell = transaction.amount;
+    let acquisition_cost = deduct_balance(balances, &transaction.ticker, to_sell);
+
+    RealizedProfit {
+        date: transaction.timestamp.clone(),
+        amount_sold: transaction.amount,
+        currency: transaction.ticker.clone(),
+        eur_total: transaction.subtotal,
+        cost: acquisition_cost + transaction.fees
+    }
+}
+
+fn profits_balances(transactions: &Vec<Transaction>) -> (Vec<RealizedProfit>, HashMap<String, Vec<BalanceEntry>>) {
+    let mut balances: HashMap<String, Vec<BalanceEntry>> = HashMap::new();
+    let mut profits: Vec<RealizedProfit> = Vec::new();
+    for transaction in transactions.iter() {
+        if transaction.transaction_type == transaction_types::BUY {
+            adjust_balance_buy(&mut balances, transaction);
+        } else if transaction.transaction_type == transaction_types::SELL {
+            if transaction.currency == "EUR" {
+                let realized_profit = calculate_profit(&mut balances, transaction);
+                profits.push(realized_profit);
+            } else {
+                adjust_balance_sell(&mut balances, transaction);
+            }
+        }
+    }
+    (profits, balances)
+}
+
+pub fn calculate_profits_balances(file_path: String) -> (Vec<RealizedProfit>, HashMap<String, Vec<BalanceEntry>>) {
+    let mut transactions = read_transactions(file_path).expect("Can't read transactions");
+    transactions.sort_by(|t1, t2| t1.timestamp.cmp(&t2.timestamp) );
+    profits_balances(&transactions)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn no_sell_transactions() {
+        let transactions = vec![Transaction {
+            timestamp: Utc::now(),
+            ticker: String::from("BTC"),
+            transaction_type: String::from(transaction_types::BUY),
+            amount: 1000_f64,
+            subtotal: 1000_f64,
+            fees: 1_f64,
+            total: 1001_f64,
+            currency: String::from("EUR"),
+            price_per_coin: 1_f64,
+            id: String::from("1")
+        }];
+        let (profits, balances) = profits_balances(&transactions);
+        assert!(profits.len() == 0);
+        let btc_balance = balances.get("BTC").unwrap();
+        assert!(btc_balance.len() == 1);
+        assert!(btc_balance[0].amount == 1000_f64);
+    }
+
+    #[test]
+    fn two_purchases_one_sale() {
+        let transactions = vec![Transaction {
+            timestamp: Utc::now(),
+            ticker: String::from("BTC"),
+            transaction_type: String::from(transaction_types::BUY),
+            amount: 1000_f64,
+            subtotal: 500_f64,
+            fees: 0_f64,
+            total: 500_f64,
+            currency: String::from("EUR"),
+            price_per_coin: 0.5_f64,
+            id: String::from("1")
+        }, Transaction {
+            timestamp: Utc::now(),
+            ticker: String::from("BTC"),
+            transaction_type: String::from(transaction_types::BUY),
+            amount: 500_f64,
+            subtotal: 500_f64,
+            fees: 0_f64,
+            total: 500_f64,
+            currency: String::from("EUR"),
+            price_per_coin: 1_f64,
+            id: String::from("1")
+        }, Transaction {
+            timestamp: Utc::now(),
+            ticker: String::from("BTC"),
+            transaction_type: String::from(transaction_types::SELL),
+            amount: 1200_f64,
+            subtotal: 1800_f64,
+            fees: 0_f64,
+            total: 18000_f64,
+            currency: String::from("EUR"),
+            price_per_coin: 1_f64,
+            id: String::from("1")
+        }];
+        let (profits, balances) = profits_balances(&transactions);
+        assert!(profits.len() == 1);
+        let btc_balance = balances.get("BTC").unwrap();
+        let sum: f64 = btc_balance.iter().map(|b| b.amount).sum();
+        assert_eq!(sum, 300_f64);
+        assert_eq!(btc_balance[1].cost, 300_f64);
+        let profit = &profits[0];
+        assert_eq!(profit.eur_total, 1800_f64);
+        assert_eq!(profit.cost, 500_f64 + 200_f64);
+        println!("profit: {}", profit.profit());
+        assert_eq!(profit.profit(), 1100_f64);
+    }
+
+}
